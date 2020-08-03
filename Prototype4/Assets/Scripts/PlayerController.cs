4fa2c1e1a06964a779f23a3843543a5931882234
Azim Momin <amomin@zynga.com>
2020-08-03 15:31:11 -0700

    Finished lesson 1 in week 4.

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private GameObject cameraFocalPoint = null;
+
+    private Rigidbody playerRigidbody = null;
+    private float verticalInput;
+
+    private void Start()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        playerRigidbody.AddForce(cameraFocalPoint.transform.forward * (speed * verticalInput));
+    }
+}
---
+++ Prototype4/Assets/Scripts/RotateCamera.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 75f;
+    [SerializeField] private bool useNaturalMotion = true;
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float directionModifier = useNaturalMotion ? -1 : 1;
+        transform.Rotate(Vector3.up, horizontalInput * rotationSpeed * Time.deltaTime * directionModifier);
+    }
+}
