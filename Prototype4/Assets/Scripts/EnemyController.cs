fe8ea6252cb6d20affc487989c34a780bea04063
Azim Momin <amomin@zynga.com>
2020-08-03 16:17:06 -0700

    Completed lesson 2 in week 4.

---
+++ Prototype4/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float speed = 3f;
+
+    private Rigidbody enemyRigidbody = null;
+    private GameObject playerGameObject = null;
+
+    private void Start()
+    {
+        enemyRigidbody = GetComponent<Rigidbody>();
+        playerGameObject = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 lookDirection = (playerGameObject.transform.position - transform.position).normalized;
+        enemyRigidbody.AddForce(lookDirection * speed);
+    }
+}
---
+++ Prototype4/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    [SerializeField] private GameObject objectToSpawn = null;
+    [SerializeField] private Vector2 spawnRange = Vector3.zero;
+
+    private void Start()
+    {
+        Instantiate(
+            objectToSpawn,
+            GenerateSpawnPosition(),
+            objectToSpawn.transform.rotation
+        );
+    }
+
+    private Vector3 GenerateSpawnPosition()
+    {
+        float spawnPosX = Random.Range(-spawnRange.x, spawnRange.x);
+        float spawnPosZ = Random.Range(-spawnRange.y, spawnRange.y);
+
+        // Set the X and Z to the random values we calculated. Keep the Y the
+        // same, as what is set in the prefab. Ensure the prefab's Y value is
+        // set such that it is right above the game floor.
+        return new Vector3(
+            spawnPosX,
+            objectToSpawn.transform.position.y,
+            spawnPosZ
+        );
+    }
+
+}
